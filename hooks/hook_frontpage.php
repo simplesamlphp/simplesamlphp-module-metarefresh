4df6de2275691bbe2d6903b270f8cced3d8a89ff
Tim van Dijen <tvdijen@gmail.com>
2019-05-26 11:28:13 +0100

    Use webmozart for assertions

---
+++ hooks/hook_frontpage.php
@@ -0,0 +1,20 @@
+<?php
+
+use Webmozart\Assert\Assert;
+
+/**
+ * Hook to add links to the frontpage.
+ *
+ * @param array &$links  The links on the frontpage, split into sections.
+ * @return void
+ */
+function metarefresh_hook_frontpage(&$links)
+{
+    Assert::isArray($links);
+    Assert::keyExists($links, 'links');
+
+    $links['federation'][] = [
+        'href' => SimpleSAML\Module::getModuleURL('metarefresh/fetch.php'),
+        'text' => '{metarefresh:metarefresh:frontpage_link}',
+    ];
+}
