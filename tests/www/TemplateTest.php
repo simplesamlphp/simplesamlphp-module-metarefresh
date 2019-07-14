135a39cf4b61c74f84af5913a392cf7c8f6948ae
Tim van Dijen <tim.dijen@minbzk.nl>
2019-07-14 15:54:48 +0200

    Update TemplateTest.php

--- tests/www/TemplateTest.php
+++ tests/www/TemplateTest.php
@@ -5,12 +5,12 @@
  * @author Tim van Dijen <tvdijen@gmail.com>
  * @package SimpleSAMLphp
  */
-
+ 
 namespace SimpleSAML\Test\Web;
 
 use PHPUnit\Framework\TestCase;
-use SimpleSAML\Configuration;
-use SimpleSAML\XHTML\Template;
+use SimpleSAML\Configuration as Configuration;
+use SimpleSAML\XHTML\Template as Template;
 use SimpleSAML\Module;
 use Twig\Error\SyntaxError;
 
@@ -19,18 +19,18 @@ class TemplateTest extends TestCase
     public function testSyntax()
     {
         $config = Configuration::loadFromArray([
-            'usenewui' => true,
+            'language.i18n.backend' => 'gettext/gettext',
             'module.enable' => array_fill_keys(Module::getModules(), true),
         ]);
-        Configuration::setPreLoadedConfig($config);
 
-        $basedir = dirname(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__))))))).DIRECTORY_SEPARATOR.'templates';
+        Configuration::setPreLoadedConfig($config);
+        $basedir = dirname(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR.'templates';
 
         // Base templates
         $files = array_diff(scandir($basedir), ['.', '..']);
         foreach ($files as $file) {
             if (preg_match('/.twig$/', $file)) {
-                $t = new Template($config, $file);
+                $t = new Template($config, 'monitor:'.basename($file));
                 ob_start();
                 try {
                     $t->show();
