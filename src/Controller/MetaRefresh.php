9ab1113b66d11fdfe95b7fef70b4dee1e23e76db
Tim van Dijen <tvdijen@gmail.com>
2023-10-12 17:30:28 +0200

    Constructor property promotion

--- src/Controller/MetaRefresh.php
+++ src/Controller/MetaRefresh.php
@@ -27,9 +27,6 @@ use Symfony\Component\HttpFoundation\Request;
 class MetaRefresh
 {
     /** @var \SimpleSAML\Configuration */
-    protected Configuration $config;
-
-    /** @var \SimpleSAML\Configuration */
     protected Configuration $module_config;
 
     /**
@@ -48,9 +45,8 @@ class MetaRefresh
      * @throws \Exception
      */
     public function __construct(
-        Configuration $config
+        protected Configuration $config
     ) {
-        $this->config = $config;
         $this->module_config = Configuration::getConfig('module_metarefresh.php');
         $this->authUtils = new Utils\Auth();
     }
