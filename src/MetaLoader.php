ed2eadb2df4fbfe714078c67bff96fa2059b0f71
Tim van Dijen <tvdijen@gmail.com>
2024-06-24 18:29:49 +0200

    Fix incorrect handling of configuration

--- src/MetaLoader.php
+++ src/MetaLoader.php
@@ -671,7 +671,7 @@ class MetaLoader
      */
     public function writeMetadataPdo(Configuration $globalConfig, array $config = []): void
     {
-        $metaHandler = new Metadata\MetaDataStorageHandlerPdo($globalConfig, $config);
+        $metaHandler = new Metadata\MetaDataStorageHandlerPdo($globalConfig->toArray(), $config);
 
         foreach ($this->metadata as $set => $elements) {
             foreach ($elements as $m) {
